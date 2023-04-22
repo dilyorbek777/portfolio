a859728e4eaffd538813acab59c89464829e2647
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2023-04-22 22:30:50 +0500

    second commit

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,11 +1,13 @@
 import Work from '../components/sections/Work'
 import MiniFooter from '../components/MiniFooter';
+import bgImg from '../images/bg-img.png' 
+
 
 const Blogs = () => {
   return (
     <>
       <div className='contact'>
-        <img className='bg-img' src="https://img.freepik.com/free-vector/halftone-background-with-circles_23-2148907689.jpg?w=1060&t=st=1682061493~exp=1682062093~hmac=b90890e543245dfde5b7ad858e5d5a6d4f9628985cd4c5a21320478feec9b3a6" />
+      <img className='bg-img' src={bgImg} />
 
         <Work />
         <MiniFooter/>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+
+import bgImg from '../images/bg-img.png' 
+
+
 import './contact.css'
 import MiniFooter from '../components/MiniFooter'
 import Contact from '../components/sections/Contact'
@@ -9,7 +13,8 @@ export class ContactMe extends Component {
       <>
 
         <div className='contact'>
-          <img className='bg-img' src="https://img.freepik.com/free-vector/halftone-background-with-circles_23-2148907689.jpg?w=1060&t=st=1682061493~exp=1682062093~hmac=b90890e543245dfde5b7ad858e5d5a6d4f9628985cd4c5a21320478feec9b3a6" />
+        <img className='bg-img' src={bgImg} />
+          
           
           <Contact />
           <MiniFooter/>
